eb9868d67167ff899948d73195ca398a710b462c
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-01-06 01:11:29 +0200

    Time: 0 ms (100.00%), Space: 6 MB (8.43%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int c = 0;
+        for (int i = 1 ; i <= min(a , b) ; i++)
+        {
+            if (a % i == 0 and b % i == 0)
+                c ++;
+        }
+        return c;
+    }
+};
