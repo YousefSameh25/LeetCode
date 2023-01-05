c0e54bc4da7db87ab3be450435ae96e0065f253e
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-01-06 01:56:06 +0200

    Time: 14 ms (92.17%), Space: 17.4 MB (71.15%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    bool hasGroupsSizeX(vector<int> & deck) {
-        int frq[10000 + 10] = {} , dis = 0;
+    bool hasGroupsSizeX(vector<int> & deck)
+    {
+        int f[10000 + 10] = {};
         for (auto it : deck)
-        {
-            dis += (frq[it] == 0);
-            frq[it]++;
-        }
+            f[it]++;
         
-        int frq2 [10000 + 10] = {};
+       int g = 0;
         
         for (int i = 0 ; i <= 1e4 + 9 ; i++)
-        {
-            if (!frq[i])
-                continue;
-           
-            for (int j = 1 ; j * j <= frq[i] ; j++)
-            {
-                if (frq[i] % j == 0)
-                {
-                    frq2[j]++;
-                    if (j != frq[i] / j)
-                        frq2[frq[i] / j] ++;
-                }
-            }
-        }
+            g = __gcd(g , f[i]); 
         
-        for (int i = 2 ; i <= 1e4 + 9 ; i++)
-        {
-             if (!frq2[i])
-                continue;
-            if (frq2[i] == dis)
-                return 1;
-        }   
-        return 0;
+        return (g >= 2);
     }
 };
