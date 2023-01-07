1c7e5ce849430bef8e735074e14efd2c832e3f63
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-01-07 04:14:41 +0200

    Time: 9 ms (54.75%), Space: 7.9 MB (76.98%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n)
+    {
+        vector <int> ret (n + 1 , 0);
+        for (int i = 0 ; i <= n ; i++)
+        {
+            ret [i] = ret [i >> 1] + (i & 1);
+        }
+        return ret;
+    }
+};
