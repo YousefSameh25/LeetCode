fb065ce025336bc90556b3029aabdeec353730c7
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-01-07 04:54:09 +0200

    Time: 10 ms (7.56%), Space: 11.2 MB (5.24%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    
+public:
+    int dp[101][10001];
+    bool solve (int i , int j , string &s , string& t)
+    {
+        if (i == s.size())
+            return 1;
+        
+        if (j == t.size())
+            return 0;
+        
+        int &ans = dp[i][j];
+        
+        if (~ans)
+            return ans;
+        
+        ans = 0;
+        
+        if (s[i] == t[j])
+            ans |= solve (i + 1 , j + 1 , s , t);
+        
+        else 
+        {
+            ans |= solve (i , j + 1 , s , t);
+        }
+        
+        return ans;
+        
+    }
+    bool isSubsequence(string s, string t) {
+        memset (dp , -1 , sizeof dp);
+        return solve (0 , 0 , s , t);
+    }
+};
