8ac47d969a02caec276cb50f2230ce8de0147211
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-01-26 04:48:01 +0200

    Time: 8 ms (53.16%), Space: 8.7 MB (63.81%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:
+    int vis[110];
+    vector <int> g[110];
+    void dfs (Node* node)
+    {
+        vis[node -> val] = 1;
+        for (auto child : node -> neighbors)
+        {
+            g[node -> val].push_back(child -> val);
+            if (vis[child -> val])
+                continue;
+            dfs(child);
+        }
+    }
+    Node* cloneGraph(Node* node) 
+    {  
+        if (node == NULL)
+            return NULL;
+        dfs(node);
+        map <int , Node*> mp;
+        for (int i = 0 ; i <= 109 ; i++)
+        {
+            if (vis[i] == 0)
+                continue;
+            
+            if (not mp.count(i))
+                mp[i] = new Node(i);
+            
+            for (int child : g[i])
+            {
+                if (not mp.count(child))
+                   mp[child] = new Node(child);
+                mp[i] -> neighbors.push_back(mp[child]);
+            }
+        }
+        return mp[1];
+    }
+    
+};
