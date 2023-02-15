d9dbb507df3128e29e1a91b1cfe6ebfa121fe032
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-15 17:42:27 +0200

    Time: 832 ms (25.69%), Space: 192.6 MB (25.57%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector <int> g[100000 + 10];
-    bool bad[100000 + 10];
+    set < int > bad;
     int dfs (int node , int p)
     {
         int ret = 1;
         for (auto &ch : g[node])
         {
-            if (ch == p or bad[ch])
+            if (ch == p or bad.count(ch))
                 continue;
             ret += dfs (ch , node);
         }
         return ret;
     }
-    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-        
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) 
+    {    
         for (auto it : edges)
         {
             g[it[0]].push_back(it[1]);
             g[it[1]].push_back(it[0]);
         }
         for (auto it : restricted)
-            bad[it] = 1;
+            bad.insert(it);
         
         return dfs (0 , 0);
     }
