1c7478f8bdfaf8e2769b5f9d440127aaafc2a91f
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-16 23:09:18 +0200

    Time: 27 ms (39.69%), Space: 26.1 MB (11.58%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int longestMountain(vector<int>& arr) {
-        int mx = 0;
-        for (int i = 0 ; i < arr.size() ; i++)
+    int longestMountain(vector<int>& arr) 
+    {
+        int n = arr.size() , mx = 0;
+        vector <int> l(10000 + 10 , 0) , r(10000 + 10 , 0);
+        for(int i  =1 ; i< n;i++)
         {
-            int l = 0;
-            for (int j = i - 1 ; j >= 0 ; j--){
-                if (arr[j] >= arr[j + 1])
-                    break;
-                l++;
-            }
-            int r = 0;
-            for (int j = i + 1 ; j < arr.size() ; j++){
-                if (arr[j] >= arr[j - 1])
-                    break;
-                r++;
-            }
-            if (l == 0 || r == 0)
-                continue;
-            mx = max(mx , r + l + 1);
+            if(arr[i] > arr[i-1])
+                l[i] += l[i-1] + 1;
+            else 
+                l[i] = 0;
         }
+        
+        for(int i = n - 2 ; i >= 0 ; i--)
+        {
+            if(arr[i] > arr[i+1])
+                r[i] += r[i+1] + 1;
+            else 
+                r[i] = 0;
+        }
+        
+       for(int i = 0 ; i < n ;i++)
+       {
+          if(!l[i] or !r[i])
+              continue;
+           mx = max(mx , l[i] + r[i] +1);
+       }
         return mx;
     }
 };
