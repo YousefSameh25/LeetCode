903f7a461a377bad9703ef0904caa3e55876ed60
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-19 22:21:11 +0200

    Time: 637 ms (5.03%), Space: 155.8 MB (5.07%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    set <long long> st;
-    map <long long , bool> dp;
+    vector <long long> v;
+    unordered_map <long long , bool> dp;
     void pre (long long num)
     {
         if (num >= 1e10)
             return;
         if (dp.count(num))
             return;
-        st.insert(num);
+
         pre(num * 2);
         pre(num * 3);
         pre(num * 5);
+        
+        if (not dp.count(num))
+            v.push_back(num);
+        
         dp[num] = true;
     }
     int nthUglyNumber(int n) 
     {
         pre(1LL);
-        int i = 0;
-        for (auto & it : st)
-        {
-            i++;
-            if (i == n)
-                return it;
-        }
-        return 0;
+        sort(v.begin() , v.end());
+        return v[n - 1];
     }
 };
