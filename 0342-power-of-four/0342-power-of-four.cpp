25b833c28258eff644bf466df0e6e5ec2403da76
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-22 18:15:57 +0200

    Time: 3 ms (51.54%), Space: 6.1 MB (16.90%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if (n <= 0)
             return false;
-        int x = log2(abs(n));
-        return (x == log2(abs(n)) and (int)log2(abs(n)) % 2 == 0);
+        int x = log2(n);
+        return (x == log2(n) and (int) log2(n) % 2 == 0);
     }
 };
