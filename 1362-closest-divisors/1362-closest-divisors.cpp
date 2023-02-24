4b51c65d1f30837b417b6df561ce015c59dff69a
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-24 03:31:05 +0200

    Time: 15 ms (41.55%), Space: 6.1 MB (63.38%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    vector<int> closestDivisors(int num) {
-        int mn = 1e9 , x , y;
-        num++;
+    int mn = 1e9 , x , y;
+    void div (int num)
+    {
         for (int i = 1 ; i * i <= num ; i++)
         {
             if (num % i == 0)
@@ -12,16 +12,9 @@ public:
                     mn = d, x = i , y = num / i;
             }
         }
-        num++;
-         for (int i = 1 ; i * i <= num ; i++)
-        {
-            if (num % i == 0)
-            {
-                int d = num / i - i;
-                if (mn > d)
-                    mn = d, x = i , y = num / i;
-            }
-        }
+    }
+    vector<int> closestDivisors(int num) {
+        div(num + 1) , div(num + 2);
         return {x,y};
     }
 };
