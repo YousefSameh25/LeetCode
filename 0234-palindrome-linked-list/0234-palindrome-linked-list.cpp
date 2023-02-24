7149fdbea6ecf1dd63fe1fe70e0f8a64855f5520
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-02-24 03:39:37 +0200

    Time: 266 ms (45.97%), Space: 123.4 MB (33.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector <int> v;
+        vector <short> v;
         while(head != NULL)
         {
             v.push_back(head->val);
