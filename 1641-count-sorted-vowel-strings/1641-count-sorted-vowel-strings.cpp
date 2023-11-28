d90a8ae1d9913b1f99c053cbbda35cc192407804
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2023-11-28 19:29:12 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (42.75%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -5,24 +5,28 @@ public:
     char arr[5] = {'a', 'e', 'i', 'o' , 'u'};
     int solve(int i, char lst)
     {
-        
         if (i == nn)
             return 1;
         
-        int ret = 0;
+        int &ret = dp[i][lst - 'a'];
+        
+        if (~ret)
+            return ret;
+        
+        ret = 0;
         
         for (int j = 0 ; j < 5; j++)
         {
-            if (lst <= arr[j] or lst == '*')
+            if (lst <= arr[j] or lst == 'z')
                 ret += solve(i + 1 , arr[j]);
         }
         
         return ret;
-        
     }
     
     int countVowelStrings(int n) {
+        memset (dp , -1 , sizeof(dp));
         nn = n;
-        return solve(0, '*');
+        return solve(0, 'z');
     }
 };
