71802bb93ea918878d9099e20a423b5c08f76731
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2024-01-11 23:20:04 +0200

    Time: 55 ms (38.74%), Space: 48.7 MB (64.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,6 +3,18 @@ public:
     
     bool canJump(vector<int>& nums) 
     {
+        /*
+            Idea: Greedy choice
+            we have to look at last index and ask ourself how we can reach this index?
+            answer: from the privous index which has value >= 1
+            or      from the second privous index which has value >= 2
+            or      from the third privous index which has value >= 3
+            or etc.
+            
+            using the first valid choice will face us, we will update out target.
+           
+            if at the end our target = 0 the answer is true
+        */
         int curidx = nums.size() - 1;
         int pre = curidx - 1;
         int len = 1;
