be0df07bd305caaf8f5d5bf082892b9ee6ff8c56
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2024-01-11 22:48:57 +0200

    Time: 21 ms (71.78%), Space: 26 MB (15.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // We can't store at the original array as we will lose the value in the new index.
         vector <int> newarr(nums.size());
         for (int i = 0 ; i < nums.size() ; i++)
         {
