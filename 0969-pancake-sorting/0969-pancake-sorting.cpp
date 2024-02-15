8f48c1e2835c5d8fa02d90aa9d2e63f0c10da39e
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2024-02-15 20:49:16 +0200

    Time: 0 ms (100.00%), Space: 12.8 MB (48.15%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-
-    bool isSorted(const vector<int>& arr) {
-        for (int i = 0; i < arr.size() - 1; i++)
-            if (arr[i] > arr[i + 1])
-                return false;
-        return true;
-    }
-
-    vector<int> pancakeSort(vector<int>& arr) {
-        
-        int end = arr.size();
-        vector<int> res;
-        while (!isSorted(arr)) {
-            int maxIndex = 0;
-            int maxValue = arr[0];
-            for (int i = 1; i < end; i++) {
-                if (maxValue < arr[i]) {
-                    maxValue = arr[i];
-                    maxIndex = i;
-                }
-            }
-            reverse(arr.begin(), arr.begin() + maxIndex + 1);
-            res.push_back(maxIndex + 1);
-            reverse(arr.begin(), arr.begin() + end);
-            res.push_back(end);
-            end--;
+    vector<int> pancakeSort(vector<int>& arr) 
+    {
+        vector <int> ans;
+        for (int i = arr.size() - 1 ; i >= 0 ; i--)
+        {
+             if (arr[i] != i + 1)
+             {
+                 // Find it's position
+                 int idx;
+                 for (int j = 0 ; j < i ; j++)
+                 {
+                     if (arr[j] == i + 1)
+                     {
+                         idx = j; 
+                         break;
+                     }
+                 }
+                 reverse (arr.begin() , arr.begin() + idx + 1);
+                 ans.push_back(idx + 1);
+                 reverse (arr.begin() , arr.begin() + i + 1);
+                 ans.push_back(i + 1); 
+             }
         }
-
-        return res;
-        
+        return ans;
     }
 };
