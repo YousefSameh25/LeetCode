8385a7a582909b349f3fd705ab5482e18308c47a
Yousef Sameh <84719726+YousefSameh25@users.noreply.github.com>
2024-02-16 18:09:27 +0200

    Time: 77 ms (51.76%), Space: 51.8 MB (8.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector < long long > pre(n + 1 , 0);
+        vector < int > pre(n + 1 , 0);
         
         for (int i = 1 ; i <= n ; i++)
             pre[i] = nums[i - 1] + pre[i - 1];
         
         int ans = 0;
-        for (int i = 1 ; i <= n ; i++)
+        unordered_map < int , int > mp;
+        
+        // Let's try every index to be the end of the subarray
+        // and let's find out how many starting points are suitable?
+        for (int i = 0 ; i <= n ; i++)
         {
-            for (int j = i ; j <= n ;j++)
-            {
-                ans += (pre[j] - pre[i - 1] == k);
-            }
+            ans += mp[pre[i] - k];
+            mp[pre[i]]++;
         }
         return ans;
     }
