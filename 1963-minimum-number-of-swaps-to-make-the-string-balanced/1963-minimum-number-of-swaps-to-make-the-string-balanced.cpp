bf02b4aefd4130ad5ddea32729587b4371c48a57
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-04-02 03:53:42 +0200

    Time: 217 ms (5.32%), Space: 81.6 MB (6.11%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        map < char , vector < int >> mp;
+        
+        for (int i = 0 ; i < s.size(); i++)
+            mp[s[i]].push_back(i);
+        
+        
+        stack < char > st;
+        
+        int ans = 0;
+        for (int i = 0 ; i < s.size() ; i++)
+        {
+            if (st.empty())
+            {
+                if (s[i] == '[')
+                    st.push(s[i]);
+                else 
+                {
+                    swap(s[i], s[mp['['].back()]);
+                    mp['['].pop_back();
+                    st.push(s[i]);
+                    ans++;
+                }
+            }
+            else 
+            {
+                if (s[i] == ']')
+                {
+                    if (st.top() == '[')
+                        st.pop();
+                    else 
+                    {
+                        swap(s[i], s[mp['['].back()]);
+                        mp['['].pop_back();
+                        st.push(s[i]);
+                        ans++;
+                    }
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
