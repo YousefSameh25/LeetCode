a38d1d992182c8e8ca558a0ad85017283c09aa0b
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-04-07 23:04:08 +0200

    Time: 157 ms (41.07%), Space: 32.2 MB (60.27%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int Solve(int i, int r, vector<vector<int>>& piles)
+    {
+        if (i == piles.size() or !r)
+            return 0;
+        
+        if (dp[i][r] != -1)
+            return dp[i][r];
+        
+        int ans = Solve(i + 1, r, piles);
+        
+        for (int j = 1 ; j <= r and j <= piles[i].size(); j++)
+        {
+            ans = max(ans , Solve(i + 1, r - j, piles) +  piles[i][j - 1]);
+        }
+        return dp[i][r] = ans;
+    }
+    
+    
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) 
+    {
+        int n = piles.size();
+        dp.resize(n, vector< int > (k + 1, -1));
+        
+        for (int i = 0 ; i < n ; i++)
+            for (int j = 1 ; j < piles[i].size(); j++)
+                piles[i][j] += piles[i][j - 1];
+        
+        return Solve(0, k, piles);
+    }
+};
