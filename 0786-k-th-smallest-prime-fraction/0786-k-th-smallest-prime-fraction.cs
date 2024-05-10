420634ab3ab7e09057ef63524c97ea174d7c9846
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-05-10 07:03:26 +0300

    Time: 745 ms (80.00%), Space: 177.7 MB (13.33%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cs
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cs
@@ -3,23 +3,22 @@ public class Solution {
         /*
             a/b > c/d => a * d > c * b
         */
-        List <double> list = new List<double>();
-        
-        Dictionary< double, KeyValuePair<int , int> > dic = 
-            new Dictionary< double, KeyValuePair<int , int> >();
+        List <KeyValuePair<double, KeyValuePair<int , int>>> list = 
+            new List<KeyValuePair<double, KeyValuePair<int , int>>>();
         
         for (int i = 0 ; i < arr.Length; i++)
         {
             for (int j = i + 1; j < arr.Length; j++)
             {
-                list.Add(1.0 * arr[i] / arr[j]);
-                dic.Add(1.0 * arr[i] / arr[j], new KeyValuePair<int, int>(arr[i], arr[j]));
+                list.Add( 
+                new KeyValuePair<double, KeyValuePair<int , int>>((1.0 * arr[i] / arr[j]),
+                new KeyValuePair<int , int>(arr[i], arr[j]))
+                );
             }
         }
-        list = list.OrderBy(r => r).ToList();
+        list = list.OrderBy(r => r.Key).ToList();
         
-        
-        int[] ans = new int[2] {dic[list[k - 1]].Key, dic[list[k - 1]].Value};
+        int[] ans = new int[2] {list[k - 1].Value.Key, list[k - 1].Value.Value};
         return ans;
         
     }
