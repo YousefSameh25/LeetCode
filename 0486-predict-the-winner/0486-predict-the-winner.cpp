983092598505433b0fbcd57cb109466370e7e572
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-06-06 14:47:54 +0300

    Time: 0 ms (100.00%), Space: 8.8 MB (63.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    
+    int dp[2][20][20];
     int solve(int t, int l, int r, vector<int>& nums)
     {
         if (l > r)
             return 0;
         
-        int mx;
+        int &mx = dp[t][l][r];
+        
+        if (mx != -1e9)
+            return mx;
+        
         if(!t)
         {
             mx = solve(!t, l + 1, r, nums) + nums[l];
@@ -21,7 +25,13 @@ public:
     }
     
     
-    bool predictTheWinner(vector<int>& nums) {
+    bool predictTheWinner(vector<int>& nums) 
+    {
+        for (int i = 0 ; i < 2; i++)
+            for (int j = 0 ; j < 20; j++)
+                for (int k = 0 ; k < 20; k++)
+                    dp[i][j][k] = -1e9;
+
         return solve(0, 0, nums.size() - 1, nums) >= 0;
     }
 };
