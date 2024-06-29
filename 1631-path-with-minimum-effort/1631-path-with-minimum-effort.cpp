ffdd4bca0f250fdb285971afab478a813354f128
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-06-29 19:54:15 +0300

    Time: 236 ms (11.37%), Space: 48 MB (13.98%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -9,13 +9,12 @@ public:
     }
     
     
-    int dij (int k , vector<vector<int>>& heights)
+    int dij (vector<vector<int>>& heights)
     {
         priority_queue < vector < int >> pq;
         pq.push({0, 0, 0});
         
         int dis[heights.size()][heights[0].size()];
-        pair < int , int > parent[heights.size()][heights[0].size()];
         
         for (int i = 0 ; i < heights.size(); i++)
             for (int j = 0; j < heights[0].size(); j++)
@@ -40,53 +39,22 @@ public:
                 if (!valid(ci, cj, heights.size(), heights[0].size()))
                     continue;
                 
-                if (abs(heights[it[1]][it[2]] - heights[ci][cj]) > k)
-                    continue;
-                
-                if (abs(heights[it[1]][it[2]] - heights[ci][cj]) + it[0] < dis[ci][cj])
+                int newDis = max(abs(heights[it[1]][it[2]] - heights[ci][cj]), it[0]);
+                if (newDis < dis[ci][cj])
                 {
-                    pq.push({-(abs(heights[it[1]][it[2]] - heights[ci][cj]) + it[0]), ci, cj});
-                    dis[ci][cj] = abs(heights[it[1]][it[2]] - heights[ci][cj]) + it[0];
-                    parent[ci][cj] = make_pair(it[1], it[2]);
+                    pq.push({-newDis, ci, cj});
+                    dis[ci][cj] = newDis;
                 }
             }
         }
         
         
-        if (dis[heights.size() - 1][heights[0].size() - 1] == 1e9)
-            return -1;
-        
-        int ans = -1e9;
-        int nodei = heights.size() - 1, nodej = heights[0].size() - 1;
-        
-        while(nodei or nodej)
-        {
-            auto par = parent[nodei][nodej];
-            ans = max(ans, 
-                      abs(heights[nodei][nodej] - heights[par.first][par.second]));
-            nodei = par.first;
-            nodej = par.second;
-        }
-        return ans;
+       return dis[heights.size() - 1 ][heights[0].size() - 1];
     }
     
     
     int minimumEffortPath(vector<vector<int>>& heights) {
         
-        int l = 0, r = 1e6, ans;
-        
-        while(l <= r)
-        {
-            int mid = (l + r) >> 1;
-            if (dij(mid, heights) != -1)
-            {
-                ans = mid;
-                r = mid - 1;
-            }
-            else
-                l = mid + 1;
-        }
-        
-        return ans;  
+        return dij(heights);
     }
 };
