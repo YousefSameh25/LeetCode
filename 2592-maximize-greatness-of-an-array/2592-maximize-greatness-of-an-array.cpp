f3040bb73244e21ea9633b1ce8fc307c848bcf95
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-06-29 17:52:03 +0300

    Time: 84 ms (69.96%), Space: 79.9 MB (58.33%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maximizeGreatness(vector<int>& nums) {
+        /*
         multiset < int > mst;
         for (auto it : nums)
             mst.insert(it);
@@ -13,6 +14,25 @@ public:
                 continue;
             ans++;
             mst.erase(iter);
+        }
+        return ans;
+        */
+        
+        // Another solution
+        
+        if (nums.size() == 1)
+            return 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        int l = 0, r = 1, ans = 0;
+        
+        while(l < nums.size() and r < nums.size())
+        {
+            if (nums[r] > nums[l])
+                ans++ , r++, l++;
+            else
+                r++;
         }
         return ans;
     }
