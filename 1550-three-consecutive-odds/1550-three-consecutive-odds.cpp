9af5a164ae409b80c01c382af5458210d6079890
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-07-02 09:57:22 +0300

    Time: 0 ms (100.00%), Space: 10.6 MB (8.28%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -3,7 +3,7 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         if (arr.size() < 3)
             return 0;
-        for (int i = 1 ; i <= arr.size() - 2; i++)
+        for (int i = 1 ; i < arr.size() - 1; i++)
         {
             if (arr[i - 1] % 2 != 0 and arr[i] % 2 != 0 and arr[i + 1] % 2 != 0)
                 return 1;
