1893abeae88723f2c09155015b76548ec0741143
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-07-22 10:13:24 +0300

    Time: 82 ms (95.28%), Space: 59.9 MB (93.89%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,68 +1,67 @@
 class Solution {
 public:
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
-                                    vector<vector<int>>& colConditions) {
-        // Store the topologically sorted sequences.
-        vector<int> orderRows = topoSort(rowConditions, k);
-        vector<int> orderColumns = topoSort(colConditions, k);
+    vector < int > vis, rowOrder, colOrder;
+    bool Cycle = 0;
 
-        // If no topological sort exists, return empty array.
-        if (orderRows.empty() or orderColumns.empty()) return {};
+    void DFS(int node, vector < int > G[], bool IsRow)
+    {
+        vis[node] = 1;
+        for (auto &child : G[node])
+        {
+            if (vis[child] == 2)
+                continue;
+            if (vis[child] == 1)
+            {
+                Cycle = 1;
+                return;
+            }
+            DFS(child, G, IsRow);
+        }
+        vis[node] = 2;
+        IsRow? rowOrder.push_back(node) : colOrder.push_back(node);
+    }
 
-        vector<vector<int>> matrix(k, vector<int>(k, 0));
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < k; j++) {
-                if (orderRows[i] == orderColumns[j]) {
-                    matrix[i][j] = orderRows[i];
+    void solve(int k, vector < vector < int > > & edges, bool IsRow)
+    {
+        vis.clear();
+        vis.resize(k + 1 , 0);
+        vector < int > G[k + 1];
+        for (auto &it : edges)
+            G[it[0]].push_back(it[1]);
+        
+        for (int i = 1 ; i <= k ; i++)
+        {
+            if (vis[i])
+                continue;
+            DFS(i, G, IsRow);
+        }
+    }
+
+
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) 
+    {
+        solve(k, rowConditions, 1);
+        if (Cycle)
+            return {};
+        solve(k, colConditions, 0);
+        if (Cycle)
+            return {};
+
+        reverse(rowOrder.begin(), rowOrder.end());
+        reverse(colOrder.begin(), colOrder.end());
+
+        vector < vector < int >> mat(k, vector < int > (k, 0));
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = 0; j < k ; j++)
+            {
+                if (rowOrder[i] == colOrder[j])
+                {
+                    mat[i][j] = rowOrder[i];
+                    break;
                 }
             }
         }
-        return matrix;
-    }
-
-private:
-    vector<int> topoSort(vector<vector<int>>& edges, int n) {
-        vector<vector<int>> adj(n + 1);
-        vector<int> order;
-        // 0: not visited, 1: visiting, 2: visited
-        vector<int> visited(n + 1, 0);
-        bool hasCycle = false;
-
-        // Build adjacency list
-        for (auto& x : edges) {
-            adj[x[0]].push_back(x[1]);
-        }
-
-        // Perform DFS for each node
-        for (int i = 1; i <= n; i++) {
-            if (visited[i] == 0) {
-                dfs(i, adj, visited, order, hasCycle);
-                // Return empty if cycle detected
-                if (hasCycle) return {};
-            }
-        }
-        // Reverse to get the correct order
-        reverse(order.begin(), order.end());
-        return order;
-    }
-
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited,
-             vector<int>& order, bool& hasCycle) {
-        visited[node] = 1;  // Mark node as visiting
-        for (int neighbor : adj[node]) {
-            if (visited[neighbor] == 0) {
-                dfs(neighbor, adj, visited, order, hasCycle);
-                // Early exit if a cycle is detected
-                if (hasCycle) return;
-            } else if (visited[neighbor] == 1) {
-                // Cycle detected
-                hasCycle = true;
-                return;
-            }
-        }
-        // Mark node as visited
-        visited[node] = 2;
-        // Add node to the order
-        order.push_back(node);
+        return mat;
     }
 };
