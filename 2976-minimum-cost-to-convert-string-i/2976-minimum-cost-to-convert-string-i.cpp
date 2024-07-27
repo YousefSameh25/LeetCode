c7dbfab28641caca13caee311fbda4656dbb9a88
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-07-27 17:59:28 +0300

    Time: 178 ms (58.78%), Space: 99.2 MB (47.68%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,69 +1,28 @@
 class Solution {
 public:
-
-
-    vector < long long > dij(int source , vector < vector < long long >> &mat)
-    {
-       vector < long long > distance(26 , 1e18);
-       distance[source]=0;
-
-       priority_queue < pair < long long , int >> pq;
-       pq.push({0, source});
-
-       while(pq.size())
-       {
-            auto it = pq.top();
-            pq.pop();
-            int node  = it.second;
-            long long totalCost = -it.first;
-
-            if (totalCost > distance[node])
-                continue;
-
-            for (int i = 0 ; i < 26 ; i++)
-            {
-                if (mat[node][i] == 1e18 or node == i)
-                    continue;
-                if (distance[i] >= totalCost + mat[node][i])
-                {
-                    pq.push({-(totalCost + mat[node][i]), i});
-                    distance[i] = totalCost + mat[node][i];
-                }
-            }
-       }
-       return distance;
-    }
-
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector < vector < long long >> arr (26, vector < long long> (26, 1e18));
-        for(int i=0;i<26;i++)
-        {
-            fill(begin(arr[i]),end(arr[i]), 1e18);
-            arr[i][i]=0;
-        }
-        //direct
-        for (int i =0;i<original.size();i++)
-        {
-            int x=original[i]-'a';
-            int y=changed[i]-'a';
-            long long z=cost[i];
-            arr[x][y]=min(arr[x][y],z); //noted
-        }
+        vector < vector < long long > > dis(26, vector < long long > (26, 1e18));
 
-        vector < vector < long long >> arr2 (26, vector < long long> (26, 1e18));
+        for ( int i = 0 ; i < 26 ; i++)
+            dis[i][i] = 0;
 
-        for(int i=0; i < 26;i++)
-            arr2[i] = dij(i, arr);
+        for (int i = 0 ; i < original.size(); i++)
+            dis[original[i] - 'a'][changed[i] - 'a'] = 
+            min(dis[original[i] - 'a'][changed[i] - 'a'], 1LL * cost[i]);
+
+        // Floyed Algorithm.
+        for (int k = 0; k < 26; k++)
+            for (int i = 0 ; i < 26 ; i++)
+                for (int j = 0 ; j < 26; j++)
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
 
         long long ans = 0;
-        for ( int i = 0 ; i < source.size(); i++)
+        for (int i = 0 ; i < source.size() ; i++)
         {
-            if (arr2[source[i] - 'a'][target[i] - 'a'] == 1e18)
+            if(dis[source[i] - 'a'][target[i] - 'a'] == 1e18)
                 return -1;
-            ans += arr2[source[i] - 'a'][target[i] - 'a'];
+            ans += dis[source[i] - 'a'][target[i] - 'a'];
         }
-
-
 
         return ans;
     }
