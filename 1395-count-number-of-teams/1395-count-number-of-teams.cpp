266eec9a115b1f84f0b18786f69a75e7a5c4a7ee
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-07-29 09:25:09 +0300

    Time: 314 ms (29.29%), Space: 25 MB (13.17%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[1000][1000][3];
+    int solve (int i, int lst, int len, vector<int>& rating)
+    {
+        if (i == rating.size())
+            return 0;
+
+        if (lst != -1 and dp[i][lst][len] != -1)
+            return dp[i][lst][len];
+
+        int ans = 0;
+        bool oneWay = 0;
+        if (lst == -1 or rating[i] > rating[lst])
+        {
+            if (len <= 1)
+                ans = solve(i + 1, i, len + 1, rating);
+            else if (len == 2)
+            {
+                oneWay = 1;
+                ans = solve(i + 1, lst, len, rating) + 1;
+            }
+        }
+        if (oneWay == 0)
+            ans += solve(i + 1, lst, len, rating);
+        if (lst != -1)
+            dp[i][lst][len] = ans;
+        return ans;
+    }
+
+
+    int numTeams(vector<int>& rating) {
+        memset (dp , -1, sizeof dp);
+        int ans = solve(0, -1, 0, rating);
+        reverse(rating.begin(), rating.end());
+        memset (dp , -1, sizeof dp);
+        ans += solve(0, -1, 0, rating);
+        return ans;
+    }
+};
