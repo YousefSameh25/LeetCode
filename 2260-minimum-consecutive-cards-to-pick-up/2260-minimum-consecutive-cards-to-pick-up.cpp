17255942659157c001c5ba6a1211da37052a7985
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-01 13:57:37 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        map < int , int > position;
+        int mn = INT_MAX;
+        for (int i = 0 ; i < cards.size(); i++)
+        {
+            if (!position.count(cards[i])) {
+                position[cards[i]] = i;
+                continue;
+            }
+            mn = min(mn, i - position[cards[i]] + 1);
+        }
+        return mn == INT_MAX? -1 : mn;
+    }
+};
