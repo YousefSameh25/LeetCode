0ae3aad813eb14e9eca7de3ea461144ea2bc1ce9
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-11 17:49:03 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        vector < char > suf (s.size(), 'z');
+
+        char mn = 'z';
+        for (int i = s.size() - 1 ; i >= 0; i--)
+        {
+            mn = min(mn, s[i]);
+            suf[i] = mn; 
+        }
+
+        string t, paper;
+
+        for (int i = 0 ; i < s.size() ; i++)
+        {
+            if (suf[i] == s[i])
+            {
+                t.push_back(s[i]);
+                while (i + 1 < s.size() and t.size() and t.back() < s[i + 1])
+                    paper.push_back(t.back()), t.pop_back();
+            }
+            else
+                t.push_back(s[i]);
+        }
+        while(t.size())
+            paper.push_back(t.back()), t.pop_back();
+        return paper;
+    }
+};
