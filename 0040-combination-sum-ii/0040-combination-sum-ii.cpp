0f3f3833c1b70d471b1b235a84f5e44184d259e0
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-13 11:01:38 +0300

    Time: 4 ms (60.85%), Space: 13.1 MB (34.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    
     vector < vector < int >> ans;
-    set < vector < int > > st;
     
-    void solve (int kthPos, int sum ,vector< int > &path, vector < int > &nums)
+    void solve (int kthPos, int curIdx ,int sum ,vector< int > &path, vector < int > &nums)
     {
         if (sum == 0)
         {
@@ -14,15 +12,15 @@ public:
         if (sum < 0)
             return;
 
-        for (int i = kthPos; i < nums.size(); i++)
+        for (int i = curIdx; i < nums.size(); i++)
         {
             // You are not the first number to be tried at kth position
             // and you equal the prv one.
             // that means a number before you has tried to be at kth position.
-            if (i != kthPos and nums[i] == nums[i - 1])
+            if (i != curIdx and nums[i] == nums[i - 1])
                 continue;
             path.push_back(nums[i]);
-            solve(i + 1, sum - nums[i], path, nums);
+            solve(kthPos + 1 , i + 1, sum - nums[i], path, nums);
             path.pop_back();
         }
     }
@@ -30,7 +28,7 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector < int > path;
         sort(candidates.begin(), candidates.end());
-        solve(0, target, path, candidates);
+        solve(0, 0, target, path, candidates);
         return ans;
     }
 };
