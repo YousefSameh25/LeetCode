f0b44883e0c9c6dd60f8b19235cc37734d29de66
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-14 12:57:04 +0300

    Time: 665 ms (6.62%), Space: 96.6 MB (5.05%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -2,13 +2,17 @@ class Solution {
 public: 
     int smallestDistancePair(vector<int>& nums, int k) 
     {
-        vector < int > diff (1000000 + 10 , 0);
+        vector < int > diff(1000000 + 1, 0);
+        int mxDiff = -1;
         for (int i = 0; i < nums.size(); i++)
            for (int j = i + 1 ; j < nums.size() ; j++)
+            {
                 diff[abs(nums[i] - nums[j])]++;
+                mxDiff = max(mxDiff, abs(nums[i] - nums[j]));
+            }
 
         int cnt = 0;
-        for (int i = 0 ; i <= 1000000; i++)
+        for (int i = 0 ; i <= mxDiff; i++)
         {
             cnt += diff[i];
             if (cnt >= k)
