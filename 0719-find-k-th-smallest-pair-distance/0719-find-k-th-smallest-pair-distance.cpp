192afbfc49b4f286355ce6d43c914125b840b1d0
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-14 12:50:12 +0300

    Time: 516 ms (8.52%), Space: 96.5 MB (5.05%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public: 
-
     int smallestDistancePair(vector<int>& nums, int k) 
     {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        // 1 2 2 5
-
-        
         vector < int > diff (1000000 + 10 , 0);
         for (int i = 0; i < nums.size(); i++)
            for (int j = i + 1 ; j < nums.size() ; j++)
@@ -25,3 +18,8 @@ public:
         return 404;   
     }
 };
+static const auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); 
+    return 0;
+} ();
