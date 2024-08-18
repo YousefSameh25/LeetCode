28ec98def2d51daac0cd014542479b5d5c47f501
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-18 10:19:59 +0300

    Time: 337 ms (5.05%), Space: 109.5 MB (5.1%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    vector < long long > v;
+    unordered_set < long long > dp;
+    void solve(long long num, int n)
+    {
+        if (num >= INT_MAX)
+            return;
+        if (dp.count(num))
+            return;
+        v.push_back(num);
+        solve(num * 2, n);
+        solve(num * 3, n);
+        solve(num * 5, n);
+        dp.insert(num);
+    }
     int nthUglyNumber(int n) {
-        int cnt  = 0;
-        for (int i = 1; ; i++)
-        {
-            int ii = i;
-            while(ii % 2 == 0) ii /= 2;
-            while(ii % 3 == 0) ii /= 3;
-            while(ii % 5 == 0) ii /= 5;
-            if (ii == 1)
-            {
-                cnt++;
-                if (cnt == n)
-                    return i;
-            }
-        }
-        return 404;
+        solve(1LL, n);
+        sort(v.begin(), v.end());
+        return v[n - 1];
     }
 };
