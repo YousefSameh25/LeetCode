a6be8cd03ef28e0ec18c7d9dac3609679b6c9636
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-21 10:16:10 +0300

    Time: 8 ms (96.62%), Space: 8.7 MB (84.33%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
-    int strangePrinter(string s) {
-        set < char > st;
-        for (auto it : s)
-            st.insert(it);
-        return st.size();
+    int dp[100][100];
+    int solve(int start, int end, string &s)
+    {
+        if (start > end)
+            return 0;
+
+        if (dp[start][end] != -1)
+            return dp[start][end];
+
+        // Print the first char with cost 1 and solve the remaining string.
+        int op1 = 1 + solve(start + 1, end, s);
+        
+        // Try to find a sub that has a start = end and save the end char.
+        for (int i = start + 1; i <= end; i++)
+        {
+            if (s[start] != s[i])
+                continue;
+            op1 = min(op1, solve(start, i - 1, s) + solve(i + 1, end, s));
+        }
+        return dp[start][end] = op1;
+    }
+    int strangePrinter(string s) 
+    {
+        string s2;
+        for (int i = 0 ; i < s.size(); i++)
+        {
+            if (s2.empty() or s2.back() != s[i])
+                s2.push_back(s[i]);
+        }
+        s = s2;
+        memset (dp , -1, sizeof dp);
+        return solve(0, s.size() - 1, s);
     }
 };
