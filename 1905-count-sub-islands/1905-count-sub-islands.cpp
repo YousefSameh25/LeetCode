ea8196d9b9b78a51061c4352196a0e4bb2796d90
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-08-28 20:41:19 +0300

    Time: 211 ms (69.41%), Space: 94.2 MB (79.91%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    vector<int> drow={0,1,0,-1};
-    vector<int> dcol={1,0,-1,0};
-    void dfs(vector<vector<int>> &vis,vector<vector<int>>& grid1,vector<vector<int>>& grid2,int row,int col,bool &flag){
-        vis[row][col]=1;
-        
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-
-            if(0<=nrow && nrow<grid1.size() && 0<=ncol && ncol<grid1[0].size() && grid2[nrow][ncol]==1 && !vis[nrow][ncol]){
-                if(grid1[nrow][ncol]==0){
-                    flag=false;
-                   
-                }
-                dfs(vis,grid1,grid2,nrow,ncol,flag);
-            }
-        }
-
+    bool vis[500][500];
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {1,-1,0,0};
+    bool valid(int i, int j, int n, int m)
+    {
+        return i >=0 and i < n and j >= 0 and j < m;
     }
+    bool dfs (int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2)
+    {
+        vis[i][j] = 1;
+        bool ans = 1;
+
+        for (int c = 0 ; c < 4; c++)
+        {
+            int ic = i + dx[c], jc = j + dy[c];
+            if (!valid(ic, jc, grid1.size(), grid2.back().size()))
+                continue;
+            if (!grid1[ic][jc] and grid2[ic][jc] and !vis[ic][jc])
+                ans = 0;
+            if (vis[ic][jc] or !grid2[ic][jc])
+                continue;
+            
+            ans &= dfs(ic, jc, grid1, grid2);
+        }
+        return ans;
+    }
+
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        vector<vector<int>> vis(grid1.size(),vector<int>(grid1[0].size(),0));
-        int cnt=0;
-        for(int i=0;i<grid1.size();i++){
-            for(int j=0;j<grid1[0].size();j++){
-                bool flag=true;
-                if(!vis[i][j] && grid1[i][j]==1 && grid2[i][j]==1){
-                    dfs(vis,grid1,grid2,i,j,flag);
-                    if(flag==true){
-                        cnt++;
-                    }
-                }
+        int n = grid1.size(), m = grid1.back().size(), com = 0;
+        for (int i = 0 ; i < n ; i++)
+        {
+            for (int j = 0 ; j < m; j++)
+            {
+                if (grid2[i][j] == 0 or vis[i][j] or grid1[i][j] == 0)
+                    continue;
+                com += dfs(i, j, grid1, grid2);
             }
         }
-        return cnt;
+        return com;
     }
 };
