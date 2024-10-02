fdbc2a2caba291905cf2991df2a92e2b7b4a5182
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-10-02 20:48:17 +0300

    Time: 60 ms (81.51%), Space: 43.2 MB (59.66%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         vector < int > arr2 = arr;
         sort(arr.begin(), arr.end());
-        map < int , int > mp;
+        unordered_map < int , int > mp;
         for (auto &it : arr)
         {
             if (!mp.count(it))
                 mp[it] = mp.size() + 1;
         }
         vector < int > ans;
-        for (auto &it : arr2)
-            ans.push_back(mp[it]);
-        return ans;
+        for (int i = 0 ; i < arr.size(); i++)
+            arr[i] = mp[arr2[i]];
+        return arr;
     }
 };
