faad1a4b2ad1bf89e9a37b6458bdc971be83e98f
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-10-09 14:51:47 +0300

    Time: 4 ms (18.73%), Space: 8 MB (32.94%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int minAddToMakeValid(string s) 
-    {
-        stack<char> st;
-        
-        
+    int minAddToMakeValid(string s) {
+        stack < char > st;
         for (int i = 0 ; i < s.size() ; i++)
         {
-            if (st.empty())
-                st.push(s[i]);
-            else 
+            if (s[i] == '(')
             {
-                if (s[i] == ')' and st.top() == '(')
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.size() and st.top() == '(')
                     st.pop();
-                else 
+                else
                     st.push(s[i]);
             }
         }
-        
         return st.size();
     }
 };
