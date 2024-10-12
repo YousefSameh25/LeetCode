a0cb6bec71400e9cc3718972eb3a4d672a0823f5
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-10-12 21:43:54 +0300

    Time: 301 ms (53.83%), Space: 244.9 MB (9.91%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        for (int i = 0 ; i < n ; i++)
-            swap(intervals[i][0], intervals[i][1]);
-
-        sort(intervals.begin(), intervals.end());
-
-        for (int i = 0 ; i < n ; i++)
-            swap(intervals[i][0], intervals[i][1]);
-
-        int g = 0;
-        set < pair < int , int >> st;
-        for(int i = 0 ; i < n ; i++)
+        vector < int > v(1000000 + 10, 0);
+        for (int i = 0 ; i < intervals.size() ; i++)
         {
-            if (st.empty())
-            {
-                g++;
-                st.insert({intervals[i][1], g});
-                continue;
-            }
-            auto end = st.upper_bound({intervals[i][0], -1e9});
-            while(end == st.end() or (end -> first >= intervals[i][0] and end != st.begin()))
-                end--;
-            
-            if (end -> first < intervals[i][0])
-            {
-                int tempG = end -> second;
-                st.erase(end);
-                st.insert({intervals[i][1], tempG});
-            }
-            else
-            {
-                g++;
-                st.insert({intervals[i][1], g});
-            }
+            v[intervals[i][0]]++;
+            v[intervals[i][1] + 1]--;
         }
-        return g;
+
+        int mx = v[0];
+        for(int i = 1 ; i < v.size() ; i++)
+            v[i] += v[i - 1], mx = max(mx , v[i]);
+        
+        return mx;
     }
 };
