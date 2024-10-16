71530234d5b900dbf302cb4b5c06ac8c06663023
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-10-17 00:35:59 +0300

    Time: 3 ms (42.68%), Space: 7.7 MB (63.68%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> pq;
+        // Add the counts of a, b and c in priority queue.
+        if (a > 0) {
+            pq.push({a, 'a'});
+        }
+
+        if (b > 0) {
+            pq.push({b, 'b'});
+        }
+
+        if (c > 0) {
+            pq.push({c, 'c'});
+        }
+
+        string ans = "";
+        while (!pq.empty()) {
+            pair<int, char> p = pq.top();
+            pq.pop();
+            int count = p.first;
+            char character = p.second;
+            // If three consecutive characters exist, pick the second most
+            // frequent character.
+            if (ans.length() >= 2 and ans[ans.length() - 1] == p.second and
+                ans[ans.length() - 2] == p.second) 
+            {
+
+                if (pq.empty()) break;
+
+                pair<int, char> temp = pq.top();
+                pq.pop();
+                ans.push_back(temp.second);
+
+                if (temp.first - 1 > 0) 
+                    pq.push({temp.first - 1, temp.second});
+                
+                pq.push({count, character});
+            } 
+            else 
+            {
+                count--;
+                ans.push_back(character);
+                if (count > 0) 
+                    pq.push({count, character});
+            }
+        }
+        return ans;
+    }
+};
