fe41521348588f7ef0a40e33351da7d63ddc2bfb
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-11-11 22:37:58 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool primeSubOperation(vector<int>& nums) 
+    {
+        vector < int > primes(1000 + 1, 1);
+        for ( int i = 2; i <= 1000; i++)
+        {
+            if (!primes[i])
+                continue;
+            for (int j = i + i; j <= 1000; j += i)
+            {
+                primes[j] = 0;
+            }
+        }
+
+        vector < int > pri;
+        for (int i = 2; i <= 1000 ;i++)
+        {
+            if (primes[i])
+                pri.push_back(i);       
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i == 0)
+            {
+                auto it = upper_bound(pri.begin(), pri.end(), nums[i]) - pri.begin();
+                it--;
+                if (it >= 0 and it < pri.size())
+                    nums[i] -= pri[it];
+            }
+            else
+            {
+                auto it = upper_bound(pri.begin(), pri.end(), nums[i] - nums[i - 1] - 1) - pri.begin();
+                it--;
+                if (it >= 0 and it < pri.size())
+                    nums[i] -= pri[it];
+            }
+            if (!nums[i])
+                return 0;
+        }
+        for (int i = 1 ; i < nums.size(); i++)
+        {
+            if (nums[i] <= nums[i - 1])
+                return 0;
+        }
+        return 1;
+    }
+};
