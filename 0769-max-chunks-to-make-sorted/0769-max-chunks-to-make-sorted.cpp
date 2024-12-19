2b4ef4bfdd1b89edcbdcf5b8ac17e7545b486823
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-12-19 18:20:57 +0200

    Time: 9 ms (100%), Space: 15.9 MB (7.32%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
+    int solve(int i, vector < vector < int >> &chunks, vector<int>& arr)
+    {
+        if (i == arr.size())
+        {
+            for (auto &chunk : chunks)
+                sort(chunk.begin(), chunk.end());
+            int idx = 0;
+            bool valid = 1;
+            for (auto &chunk : chunks)
+            {
+                for (auto &element : chunk)
+                {
+                    if (idx == element)
+                        idx++;
+                    else
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+            return valid? chunks.size() : 1;
+        }
+
+        // Create New chunk
+        chunks.push_back({arr[i]});
+        int op1 = solve(i + 1, chunks, arr);
+
+        // Revert the change
+        chunks.pop_back();
+
+        // If there is a size
+        if (chunks.size())
+            chunks[chunks.size() - 1].push_back(arr[i]);
+        // Empty chunks
+        else
+            chunks.push_back({arr[i]});
+            
+        int op2 = solve(i + 1, chunks, arr);
+        return max(op1, op2);
+    }
     int maxChunksToSorted(vector<int>& arr) 
     {
-        vector < pair < int , int >> ranges;
-        for (int i = 0 ; i < arr.size(); i++)
-            ranges.push_back({i, arr[i]});
-
-        int L = ranges[0].first, R = ranges[0].second, chunks = 1;
-        for (int i = 1 ; i < ranges.size(); i++)
-        {
-            if (ranges[i].first > R)
-            {
-                L = ranges[i].first;
-                R = ranges[i].second;
-                chunks++;
-            }
-            else
-            {
-                R = max(R, ranges[i].second);
-            }
-        }
-        return chunks;
+        vector < vector < int >> chunks;
+        return solve(0, chunks, arr);
     }
 };
