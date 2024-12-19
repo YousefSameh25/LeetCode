bfc12a2ec89577327275478c7b1fc3b1a1ce5a6c
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-12-19 17:52:56 +0200

    Time: 0 ms (100%), Space: 9.3 MB (7.32%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) 
+    {
+        vector < pair < int , int >> ranges;
+        for (int i = 0 ; i < arr.size(); i++)
+            ranges.push_back({i, arr[i]});
+
+        int L = ranges[0].first, R = ranges[0].second, chunks = 1;
+        for (int i = 1 ; i < ranges.size(); i++)
+        {
+            if (ranges[i].first > R)
+            {
+                L = ranges[i].first;
+                R = ranges[i].second;
+                chunks++;
+            }
+            else
+            {
+                L = min(L, ranges[i].first);
+                R = max(R, ranges[i].second);
+            }
+        }
+        return chunks;
+    }
+};
