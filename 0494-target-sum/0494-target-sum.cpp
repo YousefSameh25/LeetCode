fdb041b3d0006bd083b71face227cce123ff68d8
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-12-26 08:52:23 +0200

    Time: 54 ms (48.66%), Space: 20.4 MB (35.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,19 @@
 class Solution {
+private:
+    int dp[1000 + 1][2000 + 1];
 public:
     int findTargetSumWays(vector<int>& nums, int target, int i = 0, int sum = 0) 
     {
         if (i == nums.size())
             return sum == target;
 
-        return findTargetSumWays(nums, target, i + 1, sum + nums[i]) +
+        if (i == 0)
+            memset(dp, -1, sizeof (dp));
+
+        if (dp[i][sum + 1000] != -1)
+            return dp[i][sum + 1000];
+
+        return dp[i][sum + 1000] = findTargetSumWays(nums, target, i + 1, sum + nums[i]) +
             findTargetSumWays(nums, target, i + 1, sum - nums[i]);
     }
 };
