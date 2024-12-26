8d5743a04ddcebdec0684fac8a6c478afff2ba13
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2024-12-26 08:46:11 +0200

    Time: 908 ms (13.69%), Space: 11.6 MB (74.02%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target, int i = 0, int sum = 0) {
+        if (i == nums.size())
+        {
+            return sum == target;
+        }
+
+        return findTargetSumWays(nums, target, i + 1, sum + nums[i]) +
+            findTargetSumWays(nums, target, i + 1, sum - nums[i]);
+    }
+};
