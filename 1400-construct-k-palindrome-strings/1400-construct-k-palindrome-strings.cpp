11b38b918b3eee3fa4d2d397128caf825ab70c71
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-01-11 14:50:34 +0200

    Time: 0 ms (100%), Space: 15.1 MB (33.92%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        vector < int > frq(26, 0);
+
+        for (auto &c : s) 
+            frq[c - 'a']++;
+
+        int one = 0, two = 0;
+        for (auto &f : frq)
+        {
+            two += f / 2;
+            one += f % 2;
+        }
+
+        if (one > k)
+            return false;
+
+        k -= one;
+
+        if (k <= 0)
+            return true;
+
+        if (two >= k)
+            return true;
+        
+        return two * 2 >= k;
+    }
+};
