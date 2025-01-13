1a5f1e267c7ed9d9253a51409ff8fb21e469357b
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-01-13 23:24:03 +0200

    Time: 35 ms (56.92%), Space: 32 MB (87.31%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int minimumLength(string s) {
         vector < int > v(26, 0);
+        int ans = 0;    
         for (int i = 0 ; i < s.size(); i++)
-            v[s[i] - 'a']++;
-        int ans = 0;
-        for (int i = 0 ; i < 26; i++)
         {
-            if (!v[i])
-                continue;
-            ans += (v[i] & 1) ? 1 : 2;
-        }        
+            if (v[s[i] - 'a'] == 2)
+            {
+                v[s[i] - 'a']--;
+                ans--;
+            }
+            else
+            {
+                v[s[i] - 'a']++;
+                ans++;
+            }
+        }
+         
         return ans;
     }
 };
