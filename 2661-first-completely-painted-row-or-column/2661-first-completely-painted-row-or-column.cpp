28dc987c07dc49591cfffcd4214cd7c213b22f28
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-01-20 21:40:38 +0200

    Time: 486 ms (5.12%), Space: 506.2 MB (6.02%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size(), m = mat.back().size();
-        unordered_map < int , vector < pair < int , int >>> mp;
+        unordered_map < int , stack < pair < int , int >>> mp;
         for (int i = 0 ; i < n ; i++)
             for (int j = 0 ; j < m ; j++)
-                mp[mat[i][j]].push_back({i , j});
+                mp[mat[i][j]].push({i , j});
+
         vector < int > row(n, 0), col(m, 0);
 
         for (int i = 0 ; i < n * m ; i++)
@@ -13,13 +14,14 @@ public:
             int val = arr[i];
             while(mp[val].size())
             {
-                row[mp[val].back().first]++;
-                col[mp[val].back().second]++;
-                if (row[mp[val].back().first] == m)
+                auto pr = mp[val].top();
+                row[pr.first]++;
+                col[pr.second]++;
+                if (row[pr.first] == m)
                     return i;
-                if (col[mp[val].back().second] == n)
+                if (col[pr.second] == n)
                     return i;
-                mp[val].pop_back();
+                mp[val].pop();
             }
         }
         return 404;
