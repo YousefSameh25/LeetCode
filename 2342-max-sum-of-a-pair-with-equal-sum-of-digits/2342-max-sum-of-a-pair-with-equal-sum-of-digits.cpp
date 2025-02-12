33ae5555ac1ba8ba386e1316d738deaca6e49bcd
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-02-12 20:53:31 +0200

    Time: 34 ms (43.63%), Space: 64.5 MB (43.78%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) 
+    {
+        map < int , priority_queue < int > > mp;
+        for (auto it : nums)
+        {
+            int tempIt = it, d = 0;
+            while(it)
+            {
+                d += it % 10;
+                it /= 10;
+            }
+
+            mp[d].push(-tempIt);
+            if (mp[d].size() > 2)
+                mp[d].pop();
+        }
+        int mx = -1;
+        for (auto &st: mp)
+        {
+            if (st.second.size() != 2)
+                continue;
+            int ans = -st.second.top();
+            st.second.pop();
+            ans += -st.second.top();
+            mx = max(mx , ans);
+        }
+        return mx;
+    }
+};
