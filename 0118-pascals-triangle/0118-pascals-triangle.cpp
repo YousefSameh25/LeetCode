91cb3286b92f3cf3bc18959c04be52e95f70c009
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-08-01 23:37:38 +0300

    Time: 0 ms (100%), Space: 9.8 MB (30.64%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,26 +1,20 @@
 class Solution {
-
-private:
-    int getValue(int i, int j, vector < vector < int >> &pascal)
-    {
-        return (i >= 0 and i < pascal.size() and j >= 0 and j < pascal[i].size()) ? pascal[i][j] : 0;
-    }
-
 public:
     vector<vector<int>> generate(int numRows) 
     {
-        vector < vector < int >> pascal(numRows);
-
-        for (int i = 0 ; i < numRows; i++)
+        vector <vector < int >> ret;
+        for (int i = 1 ; i <= numRows ; i++)
         {
-            pascal[i].resize(i + 1);
-            for (int j = 0 ; j < pascal[i].size(); j++)
+            vector < int > t ;
+            for (int j = 1 ; j <= i ; j++)
             {
-                int val = getValue(i - 1, j, pascal) + getValue(i - 1, j - 1, pascal);
-                val = max(val , 1);
-                pascal[i][j] = val;
+                if (j == 1 or j == i)
+                    t.push_back(1);
+                else
+                    t.push_back(ret [i - 2][j - 2] + ret [i - 2][j - 1]);
             }
+            ret.push_back(t);
         }
-        return pascal;
+        return ret;
     }
 };
