35bfd5427fb40a11c65e7ea9459340df5f5932fe
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-08-13 23:59:26 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool isPowerOfThree(int n) 
     {   
-        int l = 0, r = 20;
+        int l = 0, r = 18;
         while(l <= r)
         {
             int mid = (l + r) >> 1;
-            long long ans = pow(3,mid);
+            long long ans = pow(3, mid);
             if (ans == n)
                 return true;
             if (ans > n)
