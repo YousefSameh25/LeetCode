b348dfe18d9c3c49431d9ef6be83503f6a89d30c
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-08-17 01:01:52 +0300

    Time: 0 ms (100%), Space: 8 MB (71.58%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,17 +1,49 @@
 using namespace std;
 class Solution {
 public:
+// 60000
+// 9669 + 300
     int maximum69Number (int num) 
     {
-        string s = to_string(num);
-        for(char &c : num)
+        int MOD = 1e5;
+        while(MOD >= 10)
         {
-            if (c == '6')
+            int rem = num % MOD; 
+            if (rem < 10)
             {
-                c = '9';
-                break;
+                if (rem == 6)
+                {
+                    num += 3;
+                    break;
+                }
             }
+            else if (rem < 100)
+            {
+                if (rem < 90)
+                {
+                    num += 30;
+                    break;
+                }
+            }
+            else if (rem < 1000)
+            {
+                if (rem < 900)
+                {
+                    num += 300;
+                    break;
+                }
+            }
+            else 
+            {
+                if (rem < 9000)
+                {
+                    num += 3000;
+                    break;
+                }
+            }
+
+            MOD /= 10;
         }
-        return stoi(s);
+        return num;
     }
 };
