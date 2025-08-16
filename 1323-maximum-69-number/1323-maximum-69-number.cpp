515c988257957ba1279717df644c3bc26f0c0e41
Yousef Sameh Mahmoud <84719726+YousefSameh25@users.noreply.github.com>
2025-08-17 00:38:36 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,11 +1,12 @@
+using namespace std;
 class Solution {
 public:
     int maximum69Number (int num) 
     {
-        string s = to_string (num);
-        for (char& c : s)
+        string s = to_string(num);
+        for(char &c : num)
         {
-            if (c =='6')
+            if (c == '6')
             {
                 c = '9';
                 break;
